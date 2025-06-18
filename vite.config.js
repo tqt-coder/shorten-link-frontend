9adf8fe235d613f8c25bffb2ed737538a679afee
Quốc Tuấn <76949884+tqt-coder@users.noreply.github.com>
2025-06-18 14:40:24 +0700

    override content

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+dotenv.config()
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BASE_URL,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
+  
 })
+console.log('REACT_APP_BASE_URL:', process.env.REACT_APP_BASE_URL);
